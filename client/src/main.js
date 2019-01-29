1655f821c9a9b533d155e76b036f8ebf9db613f3
chenjie.ma <295526320@qq.com>
2019-01-29 10:00:54 +0800

    add challenge mode

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,20 @@
 import Vue from "vue";
+import VueRouter from  "vue-router";
 import game from "./components/game";
+import ChallengeGame from "./components/challenge-game";
+
+Vue.use(VueRouter);
+let routes = [
+    {path: "/", component: game},
+    {path: "/challenge-game", component: ChallengeGame},
+];
+let router = new VueRouter({
+    routes,
+});
 
 new Vue({
     el:"#app",
+    router,
     components:{
-        game,
     }
-})
+});
