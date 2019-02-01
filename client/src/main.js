71fb1c517d51f05b621213c4fe6bb590da533ae9
chenjie.ma <295526320@qq.com>
2019-02-01 10:25:07 +0800

    add challenge mode

--- client/src/main.js
+++ client/src/main.js
@@ -2,19 +2,39 @@ import Vue from "vue";
 import VueRouter from  "vue-router";
 import game from "./components/game";
 import ChallengeGame from "./components/challenge-game";
+import AskForRate from "./components/ask-for-rate";
+
+function restoreHomeUrl() {
+    let data = localStorage.getItem("_2048_game_latest_path");
+    let homeUrl = "/classic-game";
+    if (data) {
+        homeUrl = data;
+    }
+
+    return homeUrl;
+}
+
+function storeHomeUrl() {
+    localStorage.setItem("_2048_game_latest_path", router.currentRoute.path);
+}
 
 Vue.use(VueRouter);
 let routes = [
-    {path: "/", component: game},
+    {path: "/", redirect: restoreHomeUrl()},
+    {path: "/classic-game", component: game},
     {path: "/challenge-game", component: ChallengeGame},
 ];
 let router = new VueRouter({
     routes,
 });
+router.afterEach(function () {
+    storeHomeUrl();
+});
 
 new Vue({
     el:"#app",
     router,
-    components:{
+    components: {
+        AskForRate,
     }
 });
