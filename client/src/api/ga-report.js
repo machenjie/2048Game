75f4dddced06d0b2cae507aa18d4b9fcdefc3e57
chenjie.ma <295526320@qq.com>
2019-03-20 08:24:02 +0800

    add ga

--- client/src/api/ga-report.js
+++ client/src/api/ga-report.js
@@ -1,10 +1,29 @@
 const TID = "UA-136518617-1";
 
-export default function (eventCategory, eventAction, eventLable, eventValue) {
-    $.post("http://www.google-analytics.com//collect",{
+function GaReport (uuid, vid, eventCategory, eventAction, eventLabel = "default", eventValue = "0") {
+    $.post("https://www.google-analytics.com/collect",{
         v: 1,
         t: "event",
-        tid: TID,
-
+        tid: encodeURI(TID),
+        cid: encodeURI(uuid),
+        an: "2048Game",
+        av: encodeURI(vid),
+        ec: encodeURI(eventCategory),
+        ea: encodeURI(eventAction),
+        el: encodeURI(eventLabel),
+        ev: encodeURI(eventValue)
     })
 }
+
+const CategoryActions = {
+    GA_APP_OPERATION: {
+        name: "GA_APP_OPERATION",
+        actions: {
+            OPERATION_START: "OPERATION_START",
+            OPERATION_HEARTBEAT: "OPERATION_HEARTBEAT",
+        }
+    },
+}
+
+export {GaReport as default, CategoryActions};
+
