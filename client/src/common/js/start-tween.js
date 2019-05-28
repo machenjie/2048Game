4eb9779acf28f3b991d272e727505df171a99914
chenjie.ma <295526320@qq.com>
2019-05-28 08:40:10 +0800

    add ga

--- client/src/common/js/start-tween.js
+++ client/src/common/js/start-tween.js
@@ -3,31 +3,26 @@ import TWEEN from "@tweenjs/tween.js";
 
 let _animateStart = false;
 function animate() {
-    if (!_animateStart) {
+    if (TWEEN.update()) {
         requestAnimationFrame(animate);
-        _animateStart = true;
     }
     else {
-        if (TWEEN.update()) {
-            requestAnimationFrame(animate);
-        }
-        else {
-            _animateStart = false;
-        }
+        _animateStart = false;
     }
 }
 
 export default function (startObj, endObj, updateEvent, duration = 1000, endEvent = null, startEvent = null, delay = 0, mode = TWEEN.Easing.Linear.None) {
     new TWEEN.Tween(startObj)
-        .to(endObj, duration)
-        .easing(mode)
-        .onStart(startEvent)
-        .onUpdate(updateEvent)
-        .onComplete(endEvent)
-        .delay(delay)
-        .start();
+      .to(endObj, duration)
+      .easing(mode)
+      .onStart(startEvent)
+      .onUpdate(updateEvent)
+      .onComplete(endEvent)
+      .delay(delay)
+      .start();
 
     if (!_animateStart){
+        _animateStart = true;
         animate();
     }
 };
